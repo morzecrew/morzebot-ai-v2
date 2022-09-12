7066f359d3a1be387992fffc90dfdf9e88f04510
mkovalevski <kovalevski.m@yandex.ru>
2022-09-12 21:39:14 +0300

    feature(natasha): support natasha ai lib

--- intent_catcher/intent_catcher.py
+++ intent_catcher/intent_catcher.py
@@ -1,3 +1,28 @@
+import json
+
+
 class IntentCatcher:
     def __init__(self):
         pass
+
+    def catch(self):
+        pass
+
+
+DATA_PATH = __file__ + "../data/"
+
+
+class NatashaCatcher(IntentCatcher):
+    def __init__(self, normal_sentence):
+        self.normal_sentence = normal_sentence
+
+    def catch(self):
+        intents = self._read_json()
+
+        for key, value in intents:
+            # FIXME
+            if value in self.normal_sentence.values():
+                return key
+
+    def _read_json(self):
+        return json.loads(DATA_PATH)
--- morph_tagging/tagger.py
+++ morph_tagging/tagger.py
@@ -20,3 +20,50 @@ from natasha import (
 class Tagger:
     def __init__(self):
         pass
+
+
+class Tools:
+    def __init__(self, segmenter, morph_vocab, emb):
+        self.segmenter = segmenter
+        self.morph_vocab = morph_vocab
+        self.emb = emb
+
+        self.morph_tagger = NewsMorphTagger(emb)
+        self.syntax_parser = NewsSyntaxParser(emb)
+        self.ner_tagger = NewsNERTagger(emb)
+
+
+class Extractor:
+    def __init__(self, tools: Tools):
+        self.names_extractor = NamesExtractor(tools.morph_vocab)
+        self.dates_extractor = DatesExtractor(tools.morph_vocab)
+        self.money_extractor = MoneyExtractor(tools.morph_vocab)
+        self.addr_extractor = AddrExtractor(tools.morph_vocab)
+
+
+class DocParser:
+    def __init__(self, tools: Tools, text):
+        self.doc = Doc(text)
+        self.tools = tools
+
+    def parse(self):
+        self.doc.segment(self.tools.segmenter)
+        self.doc.tag_morph(self.tools.morph_tagger)
+        self.doc.parse_syntax(self.tools.syntax_parser)
+        self.doc.tag_ner(self.tools.ner_tagger)
+        return self.doc
+
+
+class Normalizer:
+    def __init__(self, tools: Tools, doc_parser: DocParser):
+        self.doc_parser = doc_parser
+        self.tools = tools
+
+    def normalize(self):
+        doc = self.doc_parser.doc
+        for token in doc.tokens:
+            token.lemmatize(self.tools.morph_vocab)
+        # for span in doc.spans:
+        #     span.normalize(self.tools.morph_vocab)
+        # TODO: RESPONSE
+        return {_.text: _.lemma for _ in doc.tokens}
