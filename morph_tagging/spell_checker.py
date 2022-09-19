347012b666c19626282819816a05881106268886
Igor <igoranchikkuzmin@yandex.ru>
2022-09-20 02:37:25 +0300

    feature(spell_checker): add jamspell intallation guide to README.md and layout swapper

--- morph_tagging/spell_checker.py
+++ morph_tagging/spell_checker.py
@@ -1,19 +1,15 @@
-# from spellchecker import SpellChecker
-#
-# spell = SpellChecker(language='ru')
 import jamspell
+
+from morph_tagging import layout_swapper
 
 spell = jamspell.TSpellCorrector()
 # https://drive.google.com/open?id=11Qko7W4FVHdcXnKE1G45PsY1df34GwgM
 spell.LoadLangModel('../morph_tagging/jamspell_ru_model_subtitles.bin')
 
 
-# TODO: add in exceptions "морзе", because spellchecker corrects it into "море"
-
 class SpellCorrector:
     def __init__(self):
         pass
 
-    # FIXME: find new library for correcting text
     def correct(self, sentence: str):
-        return ' '.join([spell.FixFragment(word) for word in sentence.split()])
+        return ' '.join([spell.FixFragment(layout_swapper.swap_engrus(word)) for word in sentence.split()])
