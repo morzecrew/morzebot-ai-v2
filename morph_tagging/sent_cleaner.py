013943491aa6df143b8cf6aca5ae20318ea8c365
Igor <igoranchikkuzmin@yandex.ru>
2022-11-03 02:26:14 +0300

    minor(feat/faq): sent_cleaner update

--- morph_tagging/sent_cleaner.py
+++ morph_tagging/sent_cleaner.py
@@ -2,6 +2,8 @@ import re
 from nltk.corpus import stopwords
 
 DATA = set(stopwords.words('russian'))
+# https://regex101.com/
+regular_expression = r'(\w+:\/\/\S+)|(@\[А-Яа-яёЁ0-9])|(\b[^(ивксояИВКСОЯ)]{1}\b )|([^0-9А-Яа-яёЁ[^-] \t])|^rt|http.+?'
 
 
 class SentenceCleaner:
@@ -12,6 +14,9 @@ class SentenceCleaner:
         if lower:
             sentence = sentence.lower().strip()
         if stopwords:
-            return ' '.join([w for w in re.sub(r'(.)(\/)\W*', '', sentence).split() if w not in DATA])
+            # (\w+:\/\/\S+)|(@\[А-Яа-яёЁ0-9])|(\b[^(ивксояИВКСОЯ)]{1}\b )|([^0-9А-Яа-яёЁ \t])|^rt|http.+?
+            return ' '.join([w for w in re.sub(
+                regular_expression, '',
+                sentence).split() if w not in DATA])
         else:
-            return re.sub(r'(.)(\/)\W*', '', sentence)
+            return re.sub(regular_expression, '', sentence)
