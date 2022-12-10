23b1f5e69ba0a831798b7180ff3820a7b22baec3
alexander <storozhenko@sibext.com>
2022-12-10 20:44:25 +0800

    fix(db): added pass

--- db/db.py
+++ db/db.py
@@ -3,7 +3,10 @@ import os
 
 MONGODB_HOST = os.getenv('MONGO_HOST', default='localhost')
 MONGODB_PORT = os.getenv('MONGO_PORT', default=27017)
+MONGODB_USER = os.getenv('MONGO_USER', default='root')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD', default='root')
+
 DB_NAME = os.getenv('DB_NAME', default='mz_bot_ai') 
 
-connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
+connection = MongoClient(f'mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}:{MONGO_PORT}/{DB_NAME}')
 db = connection[DB_NAME]
