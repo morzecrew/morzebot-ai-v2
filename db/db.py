a119ca00930b0f04d93ce51a6c4e7583ee389e30
alexander <storozhenko@sibext.com>
2022-12-10 20:12:18 +0800

    fix(db): get settings from env

--- db/db.py
+++ db/db.py
@@ -1,8 +1,9 @@
 from pymongo import MongoClient
+import os 
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DB_NAME = 'mz_bot_ai'
+MONGODB_HOST = os.getenv('MONGO_HOST', default='localhost')
+MONGODB_PORT = os.getenv('MONGO_PORT', default=27017)
+DB_NAME = os.getenv('DB_NAME', default='mz_bot_ai') 
 
 connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
 db = connection[DB_NAME]
