67c414c468c12e2a35d48bc21d0730e114de98db
alexander <storozhenko@sibext.com>
2022-12-11 20:56:00 +0800

    minor(db): var name fixes

--- db/db.py
+++ db/db.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 import os 
 
-MONGODB_HOST = os.getenv('MONGO_HOST', default='localhost')
-MONGODB_PORT = os.getenv('MONGO_PORT', default=27017)
-MONGODB_USER = os.getenv('MONGO_USER', default='root')
-MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD', default='root')
+MONGO_HOST = os.getenv('MONGO_HOST', default='localhost')
+MONGO_PORT = os.getenv('MONGO_PORT', default=27017)
+MONGO_USER = os.getenv('MONGO_USER', default='root')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD', default='root')
 
 DB_NAME = os.getenv('DB_NAME', default='mz_bot_ai') 
 
-connection = MongoClient(f'mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}:{MONGO_PORT}/{DB_NAME}')
+connection = MongoClient(f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{DB_NAME}')
 db = connection[DB_NAME]
