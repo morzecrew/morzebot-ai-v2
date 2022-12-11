da3bcc946e34343667cd21cf06dcff484db0af81
alexander <storozhenko@sibext.com>
2022-12-11 21:53:11 +0800

    minor(db): db name fix

--- db/db.py
+++ db/db.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 import os 
 
 MONGO_URL = os.getenv('MONGO_URL', default='mongodb://localhost:27017/mz_bot_ai')
+DB_NAME = os.getenv('MONGO_DB_NAME', default='mz_bot_ai')
 
 connection = MongoClient(MONGO_URL)
 db = connection[DB_NAME]
