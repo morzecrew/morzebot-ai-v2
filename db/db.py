0ebccb181b05c636a8b3f053c284b7d1d3fdd896
alexander <storozhenko@sibext.com>
2022-12-11 21:40:48 +0800

    minor(db): more easier

--- db/db.py
+++ db/db.py
@@ -1,12 +1,7 @@
 from pymongo import MongoClient
 import os 
 
-MONGO_HOST = os.getenv('MONGO_HOST', default='localhost')
-MONGO_PORT = os.getenv('MONGO_PORT', default=27017)
-MONGO_USER = os.getenv('MONGO_USER', default='root')
-MONGO_PASSWORD = os.getenv('MONGO_PASSWORD', default='root')
+MONGO_URL = os.getenv('MONGO_URL', default='mongodb://localhost:27017/mz_bot_ai')
 
-DB_NAME = os.getenv('DB_NAME', default='mz_bot_ai') 
-
-connection = MongoClient(f'mongodb://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{DB_NAME}')
+connection = MongoClient(MONGO_URL)
 db = connection[DB_NAME]
