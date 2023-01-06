28744fe631e28711be4a91c9cac10dd17a0bab7a
kellkubrick <kellkubrick@gmail.com>
2023-01-06 17:52:22 +0300

    feature(catcher): fix enpoints

--- endpoints/intents.py
+++ endpoints/intents.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from intent_catcher.intent_catcher import IntentCatcher, NatashaCatcher
 from intent_catcher.variable_catcher import IdCatcher, VariableCatcher
-from morph_tagging.builder import Builder, MorphBuilder
+from morph_tagging.builder import Builder, MorphBuilder, EmbedderBuilder
 from morph_tagging.tagger import Tools
 from db.repository.settings_repository import is_uuid_exists
 
@@ -22,14 +22,19 @@ async def send_sentence(sentence: str, uuid: str):
         if not is_uuid_exists(uuid):
             return {'code': 404, 'status': 'uuid not found', 'error': f'settings not found by this uuid: {uuid}'}
 
-    tools = Tools()
-    builder: Builder = MorphBuilder(tools=tools)
-    normal_sentence = builder.build(sentence=sentence, uuid=uuid)
+    # tools = Tools()
+    # builder: Builder = MorphBuilder(tools=tools)
+    # normal_sentence = builder.build(sentence=sentence, uuid=uuid)
 
-    catcher: IntentCatcher = NatashaCatcher(normal_sentence)
-    response = catcher.catch()
+    # catcher: IntentCatcher = NatashaCatcher(normal_sentence)
+    # response = catcher.catch()
 
-    variable: IdCatcher = VariableCatcher(response, normal_sentence)
-    result = variable.get_variable()
+    # variable: IdCatcher = VariableCatcher(response, normal_sentence)
+    # result = variable.get_variable()
+
+    builder = EmbedderBuilder("tiny-bert2")
+    emb = builder.build()
+    catcher = IntentCatcher(sentence, emb)
+    result = catcher.get_intent()
 
     return result
