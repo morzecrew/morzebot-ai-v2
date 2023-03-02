ab6904bfc3207062f060ed6ee9d231d899690039
Igor <igoranchikkuzmin@yandex.ru>
2023-03-02 20:58:30 +0100

    minor(feat/chit_chat): update nn

--- chit_chat/t5_ru/t5_ru.py
+++ chit_chat/t5_ru/t5_ru.py
@@ -12,8 +12,8 @@ device = 'cpu'
 if torch.cuda.is_available():
     device = 'cuda'
 
-model = T5ForConditionalGeneration.from_pretrained('morzecrew/rut5-small-chit-chat', use_auth_token=TOKEN_HUGGINGFACE)
-tokenizer = T5Tokenizer.from_pretrained('morzecrew/rut5-small-chit-chat', use_auth_token=TOKEN_HUGGINGFACE)
+model = T5ForConditionalGeneration.from_pretrained('morzecrew/rut5-small-chit-chat-guffy-morze', use_auth_token=TOKEN_HUGGINGFACE)
+tokenizer = T5Tokenizer.from_pretrained('morzecrew/rut5-small-chit-chat-guffy-morze', use_auth_token=TOKEN_HUGGINGFACE)
 model = model.to(device)
 
 
@@ -32,9 +32,9 @@ class T5Model():
 
 
 chat_model = T5Model(model=model, tokenizer=tokenizer,
-                     do_sample=True, top_p=0.5, num_return_sequences=1,
-                     repetition_penalty=2.0,
-                     max_length=32)
+                     do_sample=True, top_p=0.95, num_return_sequences=5,
+                     repetition_penalty=3.5,
+                     max_length=128)
 
 
 class T5ChitChat(ChitChat):
@@ -52,13 +52,17 @@ class T5ChitChat(ChitChat):
                 do_sample=self.chatbot.do_sample,
                 top_p=self.chatbot.top_p,
                 num_return_sequences=self.chatbot.num_return_sequences,
-                repetition_penalty=self.chatbot.repetition_penalty,
-                max_length=self.chatbot.max_length)
-        response_list = []
+                # repetition_penalty=self.chatbot.repetition_penalty,
+                max_length=self.chatbot.max_length,
+            top_k = 50
+            )
 
         if len(hypotheses) == 1:
             return self.chatbot.tokenizer.decode(hypotheses[0], skip_special_tokens=True)
 
+        response_list = []
         for h in hypotheses:
             response_list.append(self.chatbot.tokenizer.decode(h, skip_special_tokens=True))
         return response_list
+
+print(T5ChitChat().response('кого ты любишь'))
