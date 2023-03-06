948b350d99aa797793e06abca586a39ed1ffc08d
Igor <igoranchikkuzmin@yandex.ru>
2023-03-06 15:46:44 +0100

    upd(feat/chit_chat): update test chit_chat

--- tests/chit_chat/test_chit_chat.py
+++ tests/chit_chat/test_chit_chat.py
@@ -1,7 +1,7 @@
 import pytest
 
+from nltk.translate.bleu_score import sentence_bleu
 from chit_chat.t5_ru.t5_ru import T5ChitChat
-
 
 
 @pytest.mark.parametrize("user_sentence", ["привет! Ты кто?",
@@ -11,6 +11,8 @@ from chit_chat.t5_ru.t5_ru import T5ChitChat
                                            "Здравствуйте я Егор"
                                            ])
 def test_chit_chat(user_sentence):
-    result = T5ChitChat().response(user_sentence)
-    print(result)
-    assert result != ""
+    test = T5ChitChat().response(user_sentence)
+    print(test)
+    score = sentence_bleu([user_sentence], test)
+    print(score)
+    assert score > 0.08
